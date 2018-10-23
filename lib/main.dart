93f54823f11a635d761f233ba0fab32de1215005
化十 <e_Gavin@163.com>
2018-10-23 20:57:54 +0800

    force landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wanandroid/common/GlobalConfig.dart';
 import 'package:wanandroid/pages/Application.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
+  return runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
