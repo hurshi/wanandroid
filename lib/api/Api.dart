efa35b68db4692b87321192526497cb515119a19
Hurshi <ihsruh@gmail.com>
2019-03-17 14:48:22 +0800

    fix http to https

--- lib/api/Api.dart
+++ lib/api/Api.dart
@@ -1,34 +1,27 @@
 class Api {
-  static final String HOME_BANNER = "http://www.wanandroid.com/banner/json";
-  static final String HOME_LIST = "http://www.wanandroid.com/article/list/";
-  static final String PROJECT_NEWEST =
-      "http://wanandroid.com/article/listproject/";
-  static final String PROJECT_CLASSIFY =
-      "http://www.wanandroid.com/project/tree/json";
-  static final String PROJECT_LIST = "http://www.wanandroid.com/project/list/";
-  static final String SEARCH_LIST = "http://www.wanandroid.com/article/query/";
-  static final String TREES_LIST = "http://www.wanandroid.com/tree/json";
-  static final String TREES_DETAIL_LIST =
-      "http://www.wanandroid.com/article/list/";
-  static final String MP_WECHAT_NAMES =
-      "http://wanandroid.com/wxarticle/chapters/json";
-  static final String MP_WECHAT_LIST = "http://wanandroid.com/wxarticle/list/";
+  static final String BASE_URL = "https://www.wanandroid.com/";
 
+  static final String HOME_BANNER = BASE_URL + "banner/json";
+  static final String HOME_LIST = BASE_URL + "article/list/";
+  static final String PROJECT_NEWEST = BASE_URL + "article/listproject/";
+  static final String PROJECT_CLASSIFY = BASE_URL + "project/tree/json";
+  static final String PROJECT_LIST = BASE_URL + "project/list/";
+  static final String SEARCH_LIST = BASE_URL + "article/query/";
+  static final String TREES_LIST = BASE_URL + "tree/json";
+  static final String TREES_DETAIL_LIST = BASE_URL + "article/list/";
+  static final String MP_WECHAT_NAMES = BASE_URL + "wxarticle/chapters/json";
+  static final String MP_WECHAT_LIST = BASE_URL + "wxarticle/list/";
   static final String AVATAR = "https://api.adorable.io/avatars/200/";
   static final String AVATAR_GITHUB = "https://github.com/identicons/";
   static final String AVATAR_CODING =
       "https://coding.net/static/fruit_avatar/Fruit-";
   static final String AVATAR_LEGO = "https://randomuser.me/api/portraits/lego/";
-  static final String LOGIN = "http://www.wanandroid.com/user/login";
-  static final String REGISTER = "http://www.wanandroid.com/user/register";
-  static final String COLLECTED_ARTICLE =
-      "http://www.wanandroid.com/lg/collect/list/";
-  static final String COLLECT_IN_ARTICLE =
-      "http://www.wanandroid.com/lg/collect/";
-  static final String COLLECT_OUT_ARTICLE =
-      "http://www.wanandroid.com/lg/collect/add/json";
-  static final String UNCOLLECT_ARTICLE =
-      "http://www.wanandroid.com/lg/uncollect_originId/";
+  static final String LOGIN = BASE_URL + "user/login";
+  static final String REGISTER = BASE_URL + "user/register";
+  static final String COLLECTED_ARTICLE = BASE_URL + "lg/collect/list/";
+  static final String COLLECT_IN_ARTICLE = BASE_URL + "lg/collect/";
+  static final String COLLECT_OUT_ARTICLE = BASE_URL + "lg/collect/add/json";
+  static final String UNCOLLECT_ARTICLE = BASE_URL + "lg/uncollect_originId/";
 
   static final String DEFAULT_PROJECT_IMG =
       "wanandroid.com/resources/image/pc/default_project_img.jpg";
